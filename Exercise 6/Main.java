28a63770df1dce96ef1468fad49c67c25055186d
Leopoldo Roa <166873477+LeopoldoRoa08@users.noreply.github.com>
2024-09-01 19:33:52 -0400

    a

---
+++ Exercise 6/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+    public class Main { 
+        public static void main(string args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter power base");
+        int base = .nextInt();
+
+    
+      }
+
+
+        
+
+    }
